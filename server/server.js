99ca9b09ee0245d24845cb9c9d8b2df1a878e142
Avant Baker <b.avant89@gmail.com>
2017-08-09 19:24:19 -0400

    Added the Noha theme to the repository

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 // Placed Modules in SHM Object to make file cleaner
 // Contains: Express, Logger, Morgan, BodyParser, Cookie Parser, Path, Consolidate, and HTTP
 import SERVER_HELPER_MODULES from './setup';
-
+console.log("Directory Name", SERVER_HELPER_MODULES.path.join(__dirname, '../src'));
 // Temporary Placement of Routes
 // TODO: Replace with Bootstrapper function
 import Index from './routes';
 
 // Express App
 let app = SERVER_HELPER_MODULES.exp();
+
 
 app.use(SERVER_HELPER_MODULES.lgr('dev'));
 app.use(SERVER_HELPER_MODULES.bdyPrsr.json());
@@ -16,6 +17,7 @@ app.use(SERVER_HELPER_MODULES.ckPrsr());
 
 // Temporary Placement of Static Resource Location
 app.use(SERVER_HELPER_MODULES.exp.static( SERVER_HELPER_MODULES.path.join(__dirname, './') ));
+app.use('/src', SERVER_HELPER_MODULES.exp.static( SERVER_HELPER_MODULES.path.join(__dirname,'../src' ) ));
 
 app.set('port', 8888);
 app.set('host', 'http://localhost:' + app.get('port') + '/');
@@ -25,6 +27,7 @@ app.set('view engine', 'html');
 app.set('views', SERVER_HELPER_MODULES.path.join(__dirname, './'));
 
 app.use('/', Index);
+
 
 let server = SERVER_HELPER_MODULES.http.createServer(app);
 
